adc3a2f24421418da86246b764877248495261bf
Christopher Hunt <chunt@vmware.com>
2011-10-10 14:17:10 +1100

    Added async reply support.

--- src/main/java/org/springframework/samples/springtz/TimeZoneServiceController.java
+++ src/main/java/org/springframework/samples/springtz/TimeZoneServiceController.java
@@ -23,6 +23,12 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeoutException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -51,15 +57,42 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/tz")
 public class TimeZoneServiceController {
 
+	/**
+	 * Utility class for providing a future task to handle replies
+	 * asynchronously.
+	 */
+	private class ReplyTask extends FutureTask<Message<?>> {
+		public ReplyTask(final RendezvousChannel replyChannel) {
+			super(new Callable<Message<?>>() {
+				@Override
+				public Message<?> call() throws Exception {
+					return template.receive(replyChannel);
+				}
+			});
+		}
+	}
+
 	/*
 	 * The channels we're going to use.
 	 */
 	private AbstractMessageChannel idsRequestChannel;
+
 	private AbstractMessageChannel offsetRequestChannel;
 
+	/*
+	 * How long we are wanting to wait on sends and receives.
+	 */
 	private static long sendReceiveTimeout = 2000L;
 
+	/**
+	 * The template to use for sending and receiving messages.
+	 */
 	private final MessagingTemplate template = new MessagingTemplate();
+
+	/**
+	 * A thread pool for processing replies. We'll limit this to 10 for now.
+	 */
+	private final ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
 	public TimeZoneServiceController() {
 		template.setReceiveTimeout(sendReceiveTimeout);
@@ -69,22 +102,39 @@ public class TimeZoneServiceController {
 	/**
 	 * Get the available ids.
 	 * 
-	 * @return a list of ides.
+	 * @return a list of ids.
 	 * @throws IOException
 	 *             something went wrong.
+	 * @throws ExecutionException
+	 *             thread issue.
+	 * @throws InterruptedException
+	 *             thread issue.
 	 */
 	@RequestMapping(value = "ids", method = RequestMethod.GET)
 	@ResponseBody
 	public List<?> getAvailableIDs(HttpServletResponse httpResponse)
-			throws IOException {
+			throws IOException, InterruptedException, ExecutionException,
+			TimeoutException {
+
 		List<?> result = null;
 
-		RendezvousChannel replyChannel = new RendezvousChannel();
+		// Create a channel for receiving replies on and build our message
+		// passing in this reply channel.
+		final RendezvousChannel replyChannel = new RendezvousChannel();
 		Message<String> request = MessageBuilder.withPayload("")
 				.setHeader(MessageHeaders.REPLY_CHANNEL, replyChannel).build();
-		template.send(idsRequestChannel, request);
-		Message<?> reply = template.receive(replyChannel);
 
+		// Start a task to wait on processing replies.
+		FutureTask<Message<?>> replyTask = new ReplyTask(replyChannel);
+		threadPool.execute(replyTask);
+
+		// Dispatch the request.
+		template.send(idsRequestChannel, request);
+
+		// Wait for the reply and process it if we have one. Note that we will
+		// not wait indefinitely here due to the reply task having a timeout on
+		// its template receive method.
+		Message<?> reply = replyTask.get();
 		if (reply != null) {
 			Object payload = reply.getPayload();
 			if (payload instanceof List<?>) {
@@ -92,6 +142,7 @@ public class TimeZoneServiceController {
 			}
 		}
 
+		// Pass back a 404 if we've got nothing to return.
 		if (result == null) {
 			httpResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
@@ -111,23 +162,30 @@ public class TimeZoneServiceController {
 	 * @return the model object key to use in the response.
 	 * @throws IOException
 	 *             something went wrong.
+	 * @throws ExecutionException
+	 * @throws InterruptedException
 	 */
 	@RequestMapping(value = "offset/{country}/{locality}", method = RequestMethod.GET)
 	@ResponseBody
 	public Integer getOffset(@PathVariable("country") String country,
 			@PathVariable("locality") String locality,
 			@RequestParam("when") Date when, HttpServletResponse httpResponse)
-			throws IOException {
+			throws IOException, InterruptedException, ExecutionException {
 
 		Integer result = null;
 
+		RendezvousChannel replyChannel = new RendezvousChannel();
 		String id = country + "/" + locality;
 		Message<String> request = MessageBuilder.withPayload(id)
-				.setHeader("when", when).build();
+				.setHeader("when", when)
+				.setHeader(MessageHeaders.REPLY_CHANNEL, replyChannel).build();
 
-		// TODO: Re-factor to obtain receipt independently.
-		Message<?> reply = template.sendAndReceive(offsetRequestChannel,
-				request);
+		FutureTask<Message<?>> replyTask = new ReplyTask(replyChannel);
+		threadPool.execute(replyTask);
+
+		template.send(offsetRequestChannel, request);
+
+		Message<?> reply = replyTask.get();
 		if (reply != null) {
 			Object payload = reply.getPayload();
 			if (payload instanceof Integer) {
